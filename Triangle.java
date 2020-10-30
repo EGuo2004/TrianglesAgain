88f5e663072d59ddc24fec4a0909c0ab1e499b0b
unknown <eguo20@stuy.edu>
2020-10-29 23:45:21 -0400

    Added Method: getPerimeter

--- Triangle.java
+++ Triangle.java
@@ -1,5 +1,7 @@
 public class Triangle{
-  private Point v1,v2,v3;
+  private Point v1;
+  private Point v2;
+  private Point v3;
 
   public Triangle(Point a, Point b, Point c) {
     v1 = a;
@@ -7,8 +9,12 @@ public class Triangle{
     v3 = c;
   }
   public Triangle(double x1, double y1,double x2, double y2,double x3, double y3) {
-    Point v1 = new Point(x1, y1);
-    Point v2 = new Point(x2, y2);
-    Point v3 = new Point(x3, y3);
+    v1 = new Point(x1, y1);
+    v2 = new Point(x2, y2);
+    v3 = new Point(x3, y3);
+  }
+
+  public double getPerimeter() {
+    return v1.distanceTo(v2) + v2.distanceTo(v3) + v1.distanceTo(v3);
   }
 }
